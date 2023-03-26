1d25ec305fd5adf945e0c9ce40506a6058b59eaa
Sergey <seregapanin23703@gmail.com>
2023-03-26 21:08:09 +0300

    Initial commit

---
+++ src/main/java/com/example/teamtelegrambot/TeamTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamtelegrambot/TeamTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamtelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
